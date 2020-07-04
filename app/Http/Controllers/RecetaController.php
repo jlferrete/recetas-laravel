5c78032ebdbe6daedebc1e65cab19d7c6050784c
jlferrete <joseluisferrete@gmail.com>
2020-07-04 08:31:47 +0200

    Created a migration, model and controller

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Receta;
 use Illuminate\Http\Request;
 
 class RecetaController extends Controller
@@ -40,10 +41,10 @@ class RecetaController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Receta  $receta
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Receta $receta)
     {
         //
     }
@@ -51,10 +52,10 @@ class RecetaController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Receta  $receta
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Receta $receta)
     {
         //
     }
@@ -63,10 +64,10 @@ class RecetaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Receta  $receta
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Receta $receta)
     {
         //
     }
@@ -74,10 +75,10 @@ class RecetaController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Receta  $receta
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Receta $receta)
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,3 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
