7f17c0ba315f093d57ac5c73effbc9fbbfa4cd02
jlferrete <joseluisferrete@gmail.com>
2020-07-04 09:25:27 +0200

    Form to Create Recipes - First Steps

--- resources/views/recetas/create.blade.php
+++ resources/views/recetas/create.blade.php
@@ -12,6 +12,27 @@
 
     <h2 class="text-center mb-5">Crear Nueva Receta</h2>
 
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <form method="post">
+                <div class="form-group">
+                    <label for="titulo">Titulo Receta</label>
+                    <input type="text" 
+                        name="titulo" 
+                        class="form-control"
+                        id="titulo"
+                        placeholder="Titulo Receta"
+                    />
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Agregar Receta">
+                </div>
+
+            </form>
+        </div>
+    </div>
+
  
 
 @endsection
