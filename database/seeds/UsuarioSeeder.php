d972aff0fdcb211d53564e0c3497657fd1087c77
jlferrete <joseluisferrete@gmail.com>
2020-07-28 19:21:45 +0200

    Creating Profiles from the Seeder

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsuarioSeeder extends Seeder
 {
@@ -12,21 +14,21 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        $user = User::create([
             'name' => 'Juan',
             'email' => 'correo@correo.com',
             'password' => Hash::make('12345678'),
             'url' => 'http://mypage.com',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
         ]);
-        DB::table('users')->insert([
+        $user->perfil()->create();
+
+        $user2 = User::create([
             'name' => 'Pablo',
             'email' => 'correo2@correo.com',
             'password' => Hash::make('12345678'),
-            'url' => 'http://mypage2.com',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
+            'url' => 'http://mypage.com',
         ]);
+
+        $user2->perfil()->create();
     }
 }
