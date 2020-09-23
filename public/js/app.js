7026f42c5155208166ac75cd4f6f6cb51ac6eb7f
jlferrete <joseluisferrete@gmail.com>
2020-09-23 19:45:06 +0200

    Fixed project

--- public/js/app.js
+++ public/js/app.js
@@ -74484,8 +74484,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/juandelatorre/Desktop/recetaslaravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/juandelatorre/Desktop/recetaslaravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\workspace-2020\recetaslaravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\workspace-2020\recetaslaravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
